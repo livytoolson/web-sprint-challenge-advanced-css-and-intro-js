9a10cfc2a67c268551b7a79080abc4d749f9ab25
Olivia Toolson <oliviatoolson@Olivias-MacBook-Pro.local>
2020-09-11 11:53:03 -0600

    updated Task 4

--- index.js
+++ index.js
@@ -233,11 +233,11 @@ function getArtistByIndex(array, index) {
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(array){
-  for(let i = 0; i < array.length; i++){
-    if (array.years >= 1900 && array.years <= 2000){
-      console.log(array.name);
-    } // if
+function get20s(data){
+  for(let i = 0; i < data.length; i++){
+    if (data[i].years <= 2000 && data[i].years >= 1900){
+      console.log(data[i].name);
+    } // if statement
   } // for loop
 } // function
 get20s(artists)
