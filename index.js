ff0d48d6ce64beb83c0b9c278f6eb86800b5d468
Olivia Toolson <oliviatoolson@Olivias-MacBook-Pro.local>
2020-09-11 10:59:02 -0600

    Task 7 JS

--- index.js
+++ index.js
@@ -233,12 +233,9 @@ function getArtistByIndex(array, index) {
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
 
-// const date = []
-// function get20s(){
-//   for (let i = 0; i == 1900-2000; i++);
-//   return (get20s());
-// }
-//   console.log(get20s(artists));
+function get20s(){
+
+}
 
 
 /* Task 5: Create a function called `removeArtist` that takes two arguments:
@@ -290,9 +287,13 @@ and returns an array with names of artists who painted more than 100 paintings.
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ..."Albrecht Dürer"]*/
 
-function lotsOfArt(){
-  
+function lotsOfArt(array){
+  for(let i = 0; i < array.length; i++){
+    if (array.paintings > 100)
+    return (array.name)
+  }
 }
+lotsOfArt(artists)
 
 // lotsOfArt(artists)
 
